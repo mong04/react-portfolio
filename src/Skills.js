44ff673d26649c6b1e6826757570af366bf5c9c2
Andrew Sanchez <bballbizniz@aol.com>
2017-11-06 14:59:03 -0700

    added tooltips and scrollfire

--- src/Skills.js
+++ src/Skills.js
@@ -40,15 +40,17 @@ class Skills extends Component {
                     <br/>
                     <h2>Skills</h2>
                     <Row>
-                        <Col s={4}><Card className="skill-card z-depth-4"><img className="responsive-img" src={ js } alt="JavaScript"/></Card></Col>
-                        <Col s={4}><Card className="skill-card z-depth-4"><div><img style={ nodeStyle } className="responsive-img" src={ nodejs } alt="Node.js"/></div></Card></Col>
-                        <Col s={4}><Card className="skill-card z-depth-4"><img className="responsive-img" src={ react } alt="React"/></Card></Col>
-                        <Col s={4}><Card className="skill-card z-depth-4"><img style={ gitStyle } className="responsive-img" src={ git } alt="Git"/></Card></Col>
-                        <Col s={4}><Card className="skill-card z-depth-4"><img style={ mongoStyle } className="responsive-img" src={ mongo } alt="MongoDB"/></Card></Col>
-                        <Col s={4}><Card className="skill-card z-depth-4"><img style={ sqlStyle } className="responsive-img" src={ mysql } alt="MySQL"/></Card></Col>
-                        <Col s={4}><Card className="skill-card z-depth-4"><img style={ w3cStyle } className="responsive-img" src={ w3c } alt="CSS3 & HTML5"/></Card></Col>
-                        <Col s={4}><Card className="skill-card z-depth-4"><img className="responsive-img" src={ bootstrap } alt="Bootstrap"/></Card></Col>
-                        <Col s={4}><Card className="skill-card z-depth-4"><img className="responsive-img" src={ wordpress } alt="WordPress"/></Card></Col>
+                        <ul>
+                            <li><Col s={4}><Card className="skill-card z-depth-4 tooltipped" data-position="top" data-delay="50" data-tooltip="JavaScript"><img className="responsive-img" src={ js } alt="JavaScript"/></Card></Col></li>
+                            <li><Col s={4}><Card className="skill-card z-depth-4 tooltipped" data-position="top" data-delay="50" data-tooltip="Node.js"><div><img style={ nodeStyle } className="responsive-img" src={ nodejs } alt="Node.js"/></div></Card></Col></li>
+                            <li><Col s={4}><Card className="skill-card z-depth-4 tooltipped" data-position="top" data-delay="50" data-tooltip="React"><img className="responsive-img" src={ react } alt="React"/></Card></Col></li>
+                            <li><Col s={4}><Card className="skill-card z-depth-4 tooltipped" data-position="top" data-delay="50" data-tooltip="Git"><img style={ gitStyle } className="responsive-img" src={ git } alt="Git"/></Card></Col></li>
+                            <li><Col s={4}><Card className="skill-card z-depth-4 tooltipped" data-position="top" data-delay="50" data-tooltip="MongoDB"><img style={ mongoStyle } className="responsive-img" src={ mongo } alt="MongoDB"/></Card></Col></li>
+                            <li><Col s={4}><Card className="skill-card z-depth-4 tooltipped" data-position="top" data-delay="50" data-tooltip="MySQL"><img style={ sqlStyle } className="responsive-img" src={ mysql } alt="MySQL"/></Card></Col></li>
+                            <li><Col s={4}><Card className="skill-card z-depth-4 tooltipped" data-position="top" data-delay="50" data-tooltip="CSS3 & HTML5"><img style={ w3cStyle } className="responsive-img" src={ w3c } alt="CSS3 & HTML5"/></Card></Col></li>
+                            <li><Col s={4}><Card className="skill-card z-depth-4 tooltipped" data-position="top" data-delay="50" data-tooltip="Bootstrap"><img className="responsive-img" src={ bootstrap } alt="Bootstrap"/></Card></Col></li>
+                            <li><Col s={4}><Card className="skill-card z-depth-4 tooltipped" data-position="top" data-delay="50" data-tooltip="WordPress"><img className="responsive-img" src={ wordpress } alt="WordPress"/></Card></Col></li>
+                        </ul>
                     </Row>
                     <br/>
                 </Container>
